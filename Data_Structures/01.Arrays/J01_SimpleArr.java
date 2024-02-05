5a93fa66149acd2367605daff35c06edb53489f2
md musharruf <mdmusharruf13@gmail.com>
2024-02-06 02:03:17 +0530

    read and print array

---
+++ Data_Structures/01.Arrays/J01_SimpleArr.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class J01_SimpleArr {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("enter size of array");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+
+        System.out.println("enter array elements : ");
+        for(int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Array elements are : ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
