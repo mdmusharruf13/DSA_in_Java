80344d970dd6d8574a159890d4cf5b50f88466d7
md musharruf <mdmusharruf13@gmail.com>
2024-02-06 14:37:01 +0530

    selection sort

---
+++ Algorithms/01.Sorting/S01_SelectionSort.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class S01_SelectionSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("enter size of array");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+
+        System.out.println("enter array elements : ");
+        for(int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Before sorting :");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+        int sortedArray[] = selectionSort(arr);
+
+        System.out.println("After sorting : ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static int[] selectionSort(int nums[]) {
+
+        for(int i = 0; i < nums.length-1; i++) {
+            int min = i;
+            for(int j = i+1; j < nums.length; j++) {
+                if(nums[j] < nums[min]) {
+                    min = j;
+                }
+            }
+            if(min != i) {
+                int temp = nums[i];
+                nums[i] = nums[min];
+                nums[min] = temp;
+            }
+        }
+        return nums;
+    }
+}
