fa67d2035f6dd026de18fc635ac91e59747f0abe
md musharruf <mdmusharruf13@gmail.com>
2024-02-16 22:39:30 +0530

    class name updated

--- Data_Structures/02.String/OccurrenceChar.java
+++ Data_Structures/02.String/OccurrenceChar.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class S01_OccurrenceChar {
+public class OccurrenceChar {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
