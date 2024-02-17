6339044e5f12bdab03f4c8a0e6c42d96fc2957a1
md musharruf <mdmusharruf13@gmail.com>
2024-02-17 09:59:19 +0530

    print 1 - 5 using recursion [ac]

---
+++ Algorithms/03.Recursion/NumOneToFive.java
@@ -0,0 +1,13 @@
+public class NumOneToFive {
+
+    static void printNum(int num) {
+        if(num == 0) {
+            return;
+        }
+        printNum(num-1);
+        System.out.println(num);
+    }
+    public static void main(String[] args) {
+        printNum(5);
+    }
+}
