4708b298a04e00b9c97262fd3faee7bfc618b41a
md musharruf <mdmusharruf13@gmail.com>
2024-02-17 10:04:19 +0530

    possible combination of string (ac)

---
+++ Algorithms/03.Recursion/PossibleCombination.java
@@ -0,0 +1,25 @@
+class PossibleCombination {
+
+    static String[] keypad = {".", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "uvw", "yz"};
+
+    static void printComb(String str, int index, String combination) {
+
+        if(index == str.length()) {
+            System.out.println(combination);
+            return;
+        }
+
+        char currentChar = str.charAt(index);
+        String mapping = keypad[currentChar - '0'];
+
+        for(int i = 0; i < mapping.length(); i++) {
+            printComb(str, index+1, combination + mapping.charAt(i));
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "23";
+        printComb(str, 0, "");
+    }
+
+}
