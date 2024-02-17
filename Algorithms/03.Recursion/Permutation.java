03f240daab8d4879eaf375176e4dc52bc8c84bb3
md musharruf <mdmusharruf13@gmail.com>
2024-02-17 10:05:17 +0530

    possible combination - permutation (ac)

---
+++ Algorithms/03.Recursion/Permutation.java
@@ -0,0 +1,23 @@
+class Permutation {
+
+    static void printPerm(String str, String permutation) {
+
+        if(str.length() == 0) {
+            System.out.println(permutation);
+            return;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+
+            char currChar = str.charAt(i);
+            String remStr = str.substring(0, i) + str.substring(i+1);
+
+            printPerm(remStr, permutation + currChar);
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+        printPerm(str, "");
+    }
+}
