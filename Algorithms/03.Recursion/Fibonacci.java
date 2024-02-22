57fcd8ed56f40b6d46a17b1f2ac20c120dcf9dfa
md musharruf <mdmusharruf13@gmail.com>
2024-02-22 16:21:49 +0530

    fibonacci - recursion

--- Algorithms/03.Recursion/Fibonacci.java
+++ Algorithms/03.Recursion/Fibonacci.java
@@ -1,17 +1,18 @@
 class Fibonacci {
     static void generateFibonacci(int n1, int n2, int nth) {
-        if(nth == 0) {
+        if (nth == 0) {
             return;
         }
-        System.out.println(n2);
-        generateFibonacci(n2, n1+n2, nth-1);
+        System.out.print(n2 + " ");
+        generateFibonacci(n2, n1 + n2, nth - 1);
     }
 
     public static void main(String[] args) {
         int n1 = 0;
         int n2 = 1;
         int nth = 8;
-        System.out.println(n1);
+        System.out.print(n1 + " ");
         generateFibonacci(n1, n2, nth);
+        System.out.println();
     }
 }
