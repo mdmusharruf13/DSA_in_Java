31850b102cfa06fa6435bc28b2ae32c6d391be0b
md musharruf <mdmusharruf13@gmail.com>
2024-02-29 23:41:48 +0530

    binary tree - approach1

---
+++ Data_Structures/06.Trees/BinaryTree.java
@@ -0,0 +1,96 @@
+public class BinaryTree {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static int idx = -1;
+
+    Node buildTree(int[] nodes) {
+        idx++;
+        if (nodes[idx] == -1) {
+            return null;
+        }
+
+        Node newNode = new Node(nodes[idx]);
+        newNode.left = buildTree(nodes);
+        newNode.right = buildTree(nodes);
+        return newNode;
+    }
+
+    static void preOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        System.out.print(node.data + " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    static void inOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        inOrder(node.left);
+        System.out.print(node.data + " ");
+        inOrder(node.right);
+    }
+
+    static void postOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.data + " ");
+    }
+
+    static int height(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+    static int countNodes(Node node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int leftNode = countNodes(node.left);
+        int rightNode = countNodes(node.right);
+        return leftNode + rightNode + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] nodes = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BinaryTree tree = new BinaryTree();
+
+        Node root = tree.buildTree(nodes);
+        System.out.println(root.data);
+
+        preOrder(root);
+        System.out.println();
+
+        inOrder(root);
+        System.out.println();
+
+        postOrder(root);
+        System.out.println();
+
+        System.out.println("height: " + height(root));
+        System.out.println("number of nodes: " + countNodes(root));
+    }
+}
